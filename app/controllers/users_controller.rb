d5c32e6f45eaf1ca85557bdcee224a62758a6a5f
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-03-08 10:11:12 +0000

    user show page and shelf seperation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
-    
+    @books_table = current_user.books.where(shelf: "Table")
+    @books_top = current_user.books.where(shelf: "Top Shelf")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:index, :show, :create]
-  resources :users, only: [:show] do
-    resources :libraries, only: [:index]
-  end
+  resources :users, only: [:show] 
   get '/search' => 'books#search'
 end
