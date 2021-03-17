daef536d38bed88c4096b5f0921724194a3f2604
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-03-17 12:30:42 +0000

    refactored book show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,8 +32,20 @@ module ApplicationHelper
     book["volumeInfo"]["authors"][0]
   end
 
+  def find_id(book)
+    book["id"]
+  end
+
   def find_title(book)
     book["volumeInfo"]["title"]
+  end
+
+  def find_type(book)
+    book["volumeInfo"]["industryIdentifiers"][0]["type"]
+  end
+
+  def find_isbn(book)
+    book["volumeInfo"]["industryIdentifiers"][0]["identifier"]
   end
 
   def find_book(name)
@@ -41,6 +53,18 @@ module ApplicationHelper
     uri = URI(url)
     response = Net::HTTP.get(uri)
     JSON.parse(response)
+  end
+
+  def find_average_rating(book)
+    book["volumeInfo"]["averageRating"]
+  end
+
+  def find_rating_count(book)
+    book["volumeInfo"]["ratingsCount"]
+  end
+
+  def find_category(book)
+    book["volumeInfo"]["categories"][0]
   end
 
   def trim_s(name)
